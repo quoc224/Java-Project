38eba8f984364218e1467c1b1bba26ae401b6ee7
d4rtwin <dacthinhhh@gmail.com>
2026-06-10 19:31:14 +0700

    Update RegisterReq.java

--- src/main/java/com/mangakousei/mangakousei_backend/dto/request/RegisterReq.java
+++ src/main/java/com/mangakousei/mangakousei_backend/dto/request/RegisterReq.java
@@ -2,17 +2,21 @@ package com.mangakousei.mangakousei_backend.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterReq {
     @NotBlank(message = "Full name cannot be blank")
+    @Size(max = 100, message = "Full name must not exceed 100 characters")
     private String fullName;
 
     @NotBlank(message = "Email cannot be blank")
     @Email(message = "Email format is invalid")
+    @Size(max = 255, message = "Email must not exceed 255 characters")
     private String email;
 
     @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
     private String password;
 }
